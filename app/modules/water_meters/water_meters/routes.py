4aaf7a0fef946dc6c76bb02b3ee85fc2b9de99c3
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-12-19 22:21:48 -0400

    feat: add routes to water meters

---
+++ app/modules/water_meters/water_meters/routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, status
+from app.core.response_schema import IResponse
+from app.core.database import SessionDep
+from app.modules.water_meters.water_meters.controllers import WaterMeterController
+from app.modules.water_meters.water_meters.model.schemas import (
+    WaterMeterBase,
+    WaterMeterPatch
+)
+
+router = APIRouter()
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=IResponse)
+async def create_water_meter(session: SessionDep, meter_info: WaterMeterBase):
+    return await WaterMeterController.create_meter(session, meter_info)
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=IResponse)
+async def list_water_meters(session: SessionDep):
+    return await WaterMeterController.get_all_meters(session)
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=IResponse)
+async def read_water_meter(id: int, session: SessionDep):
+    return await WaterMeterController.read_meter(id, session)
+
+@router.patch("/{id}", status_code=status.HTTP_200_OK, response_model=IResponse)
+async def patch_water_meter(id: int, session: SessionDep, meter_info: WaterMeterPatch):
+    return await WaterMeterController.patch_meter(id, session, meter_info)
+
+@router.delete("/{id}", status_code=status.HTTP_200_OK, response_model=IResponse)
+async def delete_water_meter(id: int, session: SessionDep):
+    return await WaterMeterController.delete_meter(id, session)
