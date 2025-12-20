7d6d9e2a8e4704b1d0375d561f1a535c0ca9899d
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-20 18:36:32 -0400

    fix: add schemas for members in schemas.py

---
+++ app/modules/members/model/schemas.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class MemberBase(BaseModel):
+    name: str
+    last_name: str
+    ci: str
+    phone: str
+
+class MemberResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
+    name: str
+    last_name: str
+    ci: str
+    phone: str
+    created_at: datetime
+    updated_at: datetime 
+    deleted_at: datetime | None = None
+
+class MemberPatch(BaseModel):
+    name: str | None = None
+    last_name: str | None = None
+    ci: str | None = None
+    phone: str | None = None
