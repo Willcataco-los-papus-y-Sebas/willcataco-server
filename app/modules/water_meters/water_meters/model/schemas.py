35a0bbad18bd14445b9c676c866d0acac7f9b286
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-12-19 21:43:29 -0400

    fix: fix the schema model from water meters

--- app/modules/water_meters/water_meters/model/schemas.py
+++ app/modules/water_meters/water_meters/model/schemas.py
@@ -1,20 +1,19 @@
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict
 
 class WaterMeterBase(BaseModel):
     action_id: int
-    water_reading: float = Field(..., gt=0)
-class WaterMeterCreate(WaterMeterBase):
-    pass
+    water_reading: float
+
+class WaterMeterResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    action_id: int
+    water_reading: float
+    created_at: datetime
+    updated_at: datetime | None = None
+    deleted_at: datetime | None = None
 
 class WaterMeterPatch(BaseModel):
     action_id: int | None = None
-    water_reading: float | None = Field(default=None, gt=0)
-
-class WaterMeterResponse(WaterMeterBase):
-    model_config = ConfigDict(from_attributes=True)
-    
-    id: int
-    created_at: datetime
-    updated_at: datetime
-    deleted_at: datetime | None = None
+    water_reading: float | None = None
