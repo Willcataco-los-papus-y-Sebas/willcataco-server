732c93353e31dbaa48a9a026b1cc0067df0257be
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2025-12-23 13:22:42 -0400

    feat(action-payments): register router in main API

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.modules.health.routes import router as health_router
 from app.modules.users.routes import router as user_router
 from app.modules.auth.routes import router as auth_router
+from app.modules.water_meters.action_payments.routes import router as action_payments_router
 
 router = APIRouter()
 
 router.include_router(health_router, prefix="/health", tags=["health"])
 router.include_router(user_router, prefix="/users", tags=["user"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(action_payments_router, prefix="/action-payments", tags=["action_payments"])
