7a043aa7b776ac3e231287aa0f8876eec4e341c7
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-23 10:14:26 -0400

    fix: add require roles in routes for members

--- app/modules/members/routes.py
+++ app/modules/members/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status, Query, Depends
 
 from app.core.response_schema import IResponse
 
-from app.core.dependencies import CurrentUser, RequireRoles
+from app.core.dependencies import RequireRoles
 from app.core.enums import UserRole
 from app.modules.members.controllers import MemberController
 from app.core.database import SessionDep
@@ -12,23 +12,28 @@ from app.modules.users.model.schemas import (UserPatch)
 
 router = APIRouter()
 
-@router.post("/", status_code = status.HTTP_201_CREATED, response_model = IResponse)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model = IResponse, 
+             dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))])
 async def create_member(session: SessionDep, member_info: MemberBase):
     return await MemberController.create_member(session, member_info) 
 
-@router.get("/{id}", status_code = status.HTTP_200_OK, response_model = IResponse)
-async def read_member(id: int, session: SessionDep):
-    return await MemberController.read_member(id, session)
-
-@router.patch("/{id}", status_code=status.HTTP_200_OK, response_model = IResponse)
-async def patch_info_member(session: SessionDep, id: int, member_info: MemberPatch, user_info: UserPatch):
-    return await MemberController.patch_info_member(session, id, member_info, user_info)
-
-@router.delete("/{id}", status_code=status.HTTP_200_OK, response_model= IResponse)
-async def delete_member(session: SessionDep, id: int):
-    return await MemberController.delete_member(session, id)
-
-@router.get("/", status_code= status.HTTP_200_OK, response_model = IResponse)
+@router.get("/", status_code= status.HTTP_200_OK, response_model = IResponse,
+            dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))])
 async def search_member(session: SessionDep, ci: str | None=None, last_name: str | None=None, name: str | None=None,
                         limit: int =Query(10, ge=0, le=10), offset: int=Query(0, ge=0)):
     return await MemberController.search_member(session, ci, last_name, name, limit, offset)
+
+@router.get("/{id}", status_code = status.HTTP_200_OK, response_model = IResponse,
+            dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))])
+async def read_member(id: int, session: SessionDep):
+    return await MemberController.read_member(id, session)
+
+@router.patch("/{id}", status_code=status.HTTP_200_OK, response_model = IResponse, 
+              dependencies=[Depends(RequireRoles(UserRole.ADMIN))])
+async def patch_info_member(session: SessionDep, id: int, member_info: MemberPatch, user_info: UserPatch):
+    return await MemberController.patch_info_member(session, id, member_info, user_info)
+
+@router.delete("/{id}", status_code=status.HTTP_200_OK, response_model= IResponse,
+               dependencies=[Depends(RequireRoles(UserRole.ADMIN))])
+async def delete_member(session: SessionDep, id: int):
+    return await MemberController.delete_member(session, id)
