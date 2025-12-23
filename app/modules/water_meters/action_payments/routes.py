5452c2ef1957fddeeeaf3d5b1ad7457b46614333
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2025-12-23 13:21:50 -0400

    feat(action-payments): create routes and endpoints

---
+++ app/modules/water_meters/action_payments/routes.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, status
+
+from app.core.database import SessionDep
+from app.core.dependencies import RequireRoles
+from app.core.enums import UserRole
+from app.core.response_schema import IResponse
+from app.modules.water_meters.action_payments.controllers import ActionPaymentController
+from app.modules.water_meters.action_payments.model.schemas import ActionPaymentCreate, ActionPaymentPatch
+
+router = APIRouter()
+
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=IResponse,
+    dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))],
+)
+async def create_payment(session: SessionDep, payment_info: ActionPaymentCreate):
+    return await ActionPaymentController.create_payment(session, payment_info)
+
+@router.get(
+    "/{id}",
+    status_code=status.HTTP_200_OK,
+    response_model=IResponse,
+    dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))],
+)
+async def read_payment(id: int, session: SessionDep):
+    return await ActionPaymentController.read_payment(id, session)
+
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=IResponse,
+    dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))],
+)
+async def read_all_payments(session: SessionDep, skip: int = 0, limit: int = 100):
+    return await ActionPaymentController.read_all_payments(session, skip, limit)
+
+@router.patch(
+    "/{id}",
+    status_code=status.HTTP_200_OK,
+    response_model=IResponse,
+    dependencies=[Depends(RequireRoles(UserRole.ADMIN, UserRole.STAFF))],
+)
+async def update_payment(id: int, session: SessionDep, payment_info: ActionPaymentPatch):
+    return await ActionPaymentController.update_payment(id, session, payment_info)
+
+@router.delete(
+    "/{id}",
+    status_code=status.HTTP_200_OK,
+    response_model=IResponse,
+    dependencies=[Depends(RequireRoles(UserRole.ADMIN))],
+)
+async def delete_payment(id: int, session: SessionDep):
+    return await ActionPaymentController.delete_payment(id, session)
