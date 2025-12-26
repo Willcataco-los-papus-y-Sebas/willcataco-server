f91952fcd820c2d3ea4096e8ecc3768aed680459
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-25 22:09:13 -0400

    feat: add route for get all streets and require role for each route

--- app/modules/water_meters/streets/routes.py
+++ app/modules/water_meters/streets/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, status
 
 from app.core.response_schema import IResponse
+from app.core.dependencies import RequireRoles
+from app.core.enums import UserRole
 
 from app.modules.water_meters.streets.controllers import StreetControllers
 from app.core.database import SessionDep
@@ -10,20 +12,26 @@ from app.modules.water_meters.streets.model.schemas import (StreetBase, StreetPa
 router = APIRouter()
 
 
-@router.post("/", status_code = status.HTTP_201_CREATED, response_model= IResponse)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model= IResponse, dependencies=[RequireRoles(UserRole.ADMIN, UserRole.STAFF)])
 async def create_street(session: SessionDep, street_info: StreetBase):
     return await StreetControllers.create_street(session, street_info)
 
-@router.get("/{id}", status_code= status.HTTP_200_OK, response_model= IResponse)
+
+@router.get("/", status_code= status.HTTP_200_OK, response_model= IResponse, dependencies=[RequireRoles(UserRole.ADMIN, UserRole.STAFF)])
+async def get_all_streets(session: SessionDep):
+    return await StreetControllers.get_all_streets(session)
+
+
+@router.get("/{id}", status_code= status.HTTP_200_OK, response_model= IResponse, dependencies=[RequireRoles(UserRole.ADMIN, UserRole.STAFF)])
 async def read_street(session: SessionDep, id: int):
     return await StreetControllers.read_street(session, id)
 
 
-@router.patch("/{id}", status_code= status.HTTP_200_OK, response_model= IResponse)
+@router.patch("/{id}", status_code= status.HTTP_200_OK, response_model= IResponse, dependencies=[RequireRoles(UserRole.ADMIN, UserRole.STAFF)])
 async def patch_information_street(session: SessionDep, id: int, street_info: StreetPatch):
     return await StreetControllers.patch_info_street(session, id, street_info)
 
 
-@router.delete("/{id}", status_code= status.HTTP_200_OK, response= IResponse)
+@router.delete("/{id}", status_code= status.HTTP_200_OK, response_model= IResponse, dependencies=[RequireRoles(UserRole.ADMIN, UserRole.STAFF)])
 async def delete_street(session: SessionDep, id: int):
     return await StreetControllers.delete_street(session, id)
