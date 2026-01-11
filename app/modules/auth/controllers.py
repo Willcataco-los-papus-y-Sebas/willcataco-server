80cac42b63b436901550cd825c731b23abd02b1e
zer4c <pabloazurduy@gmail.com>
2026-01-11 13:03:00 -0400

    feat: add controller for password recovery

--- app/modules/auth/controllers.py
+++ app/modules/auth/controllers.py
@@ -7,10 +7,12 @@ from app.core.config import config
 from app.core.database import SessionDep
 from app.core.dependencies import CurrentUserFromCookie, CurrentUserFlexible
 from app.modules.auth.jwt import JWTokens
-from app.modules.auth.schemas import LoginRequest
+from app.modules.auth.schemas import LoginRequest, RecoveryUser
 from app.modules.users.services import UserService
-
-
+from app.core.email import EmailSession
+from app.core.response_schema import IResponse
+from app.modules.email.schemas import EmailBase
+from app.modules.email.services import EmailService
 class AuthController:
     @staticmethod
     async def login_token(
@@ -113,3 +115,15 @@ class AuthController:
         )
         
         return {"ok": True}
+    
+    @staticmethod
+    async def recovery_account(email : RecoveryUser, session : SessionDep, session_email : EmailSession):
+        user = await UserService.get_user_by_email(email.email, session)
+        if user:
+            email_base = EmailBase(
+                recipient=email.email,
+                subject="Recuperacion de cuenta en wilcataco",
+                body=""
+            )
+            await EmailService.send_email(session_email, email_base)
+        return IResponse(detail="email received", status_code=200)
