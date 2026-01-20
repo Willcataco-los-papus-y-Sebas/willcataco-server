0a8982228f3e5a9f423c0bb7486834cfa748f098
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-20 15:24:03 -0400

    feat(email): add ExtraPaymentEmailParams schema

--- app/modules/email/schemas.py
+++ app/modules/email/schemas.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel, EmailStr
 class EmailBase(BaseModel):
     recipient: EmailStr
     subject: str
+
+class ExtraPaymentEmailParams(BaseModel):
+    recipient: EmailStr
+    subject: str
+    name: str
+    payment_date: str
+    amount: float
