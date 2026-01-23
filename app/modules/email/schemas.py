be6099f906dcc59c1621033cc9cf919bc1581f23
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-22 21:19:31 -0400

    refactor(email): update WaterBillEmailParams with strict typing

--- app/modules/email/schemas.py
+++ app/modules/email/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr
+from decimal import Decimal
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, Field
 
 class EmailBase(BaseModel):
     recipient: EmailStr
@@ -6,6 +8,6 @@ class EmailBase(BaseModel):
 
 class WaterBillEmailParams(EmailBase):
     name: str
-    reading_value: float
-    date: str
-    months_owed: int
+    reading_value: Decimal = Field(ge=0, max_digits=10, decimal_places=2)
+    date: datetime
+    months_owed: int = Field(ge=0)
