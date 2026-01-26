20ae41047465690a06439be00c054c7b7b516ff3
zer4c <pabloazurduy@gmail.com>
2026-01-26 19:44:52 -0400

    feat: add get_receipt_extra_payment function in routes and controllers in pdf module

--- app/modules/pdf_generator/controllers.py
+++ app/modules/pdf_generator/controllers.py
@@ -1,11 +1,12 @@
 from datetime import date
+
 from fastapi import HTTPException
 
 from app.core.database import SessionDep
 from app.core.dependencies import CurrentUserFlexible
 from app.core.enums import UserRole
 from app.modules.pdf_generator.services import PdfGenService
-from app.core.database import SessionDep
+
 
 class PdfGenController:
     @staticmethod
@@ -15,14 +16,13 @@ class PdfGenController:
         return await PdfGenService.get_pdf()
 
     @staticmethod
-    async def get_member_report(session: SessionDep, id: int, curr_user_flex: CurrentUserFlexible):
+    async def get_member_report(
+        session: SessionDep, id: int, curr_user_flex: CurrentUserFlexible
+    ):
         if curr_user_flex.role not in [UserRole.ADMIN, UserRole.STAFF]:
-            raise HTTPException(
-                status_code=403, 
-                detail="Insufficient privileges"
-            )   
+            raise HTTPException(status_code=403, detail="Insufficient privileges")
         return await PdfGenService.generate_member_report(session, id)
-    
+
     @staticmethod
     async def get_new_members_report(
         session: SessionDep,
@@ -37,3 +37,12 @@ class PdfGenController:
             raise HTTPException(status_code=400, detail="invalid date range")
 
         return await PdfGenService.get_new_members_report(session, start_date, end_date)
+
+    @staticmethod
+    async def get_receipt_extra_payment(
+        session: SessionDep, curr_user_flex: CurrentUserFlexible, payment_id: int
+    ):
+        if curr_user_flex.role is UserRole.MEMBER:
+            raise HTTPException(detail="user dont have privileges", status_code=401)
+
+        return await PdfGenService.get_receipt_extra_payment(session, payment_id)
