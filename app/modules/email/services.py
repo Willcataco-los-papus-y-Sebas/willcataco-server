1504a47b234c8dd6f5ce70bead48e06dc79c1b9e
zer4c <pabloazurduy@gmail.com>
2026-01-26 17:26:36 -0400

    feat: add html and parameters in services.py in email module

--- app/modules/email/services.py
+++ app/modules/email/services.py
@@ -4,10 +4,14 @@ from email.message import EmailMessage
 from app.core.config import config
 from app.core.email import EmailSession
 from app.core.templates import TemplateLoader
-from app.modules.email.schemas import EmailBase, EmailWaterReceiptBase,  WaterBillEmailParams
+from app.modules.email.schemas import (
+    EmailBase,
+    EmailWaterReceiptBase,
+    WaterBillEmailParams,
+)
+
 
 class EmailService:
-
     @staticmethod
     async def send_email(email_session: EmailSession, email: EmailBase):
         try:
@@ -58,13 +62,17 @@ class EmailService:
             message["To"] = email.recipient
             message["Subject"] = email.subject
             body = await TemplateLoader.get_template(
-                "email/internal_login.html", url=url, expire_time=str(expire_time)
+                "email/internal_login.html",
+                url=url,
+                expire_time=str(expire_time),
+                email_title="Acceso Administracion",
+                year=str(datetime.now().year),
             )
             message.set_content(body, subtype="html")
             await email_session.send_message(message)
         except Exception:
             raise
-    
+
     @staticmethod
     async def send_water_bill_email(
         email_session: EmailSession,
@@ -78,7 +86,7 @@ class EmailService:
             template_context = bill_data.model_dump()
             template_context["date"] = bill_data.date.strftime("%Y-%m-%d")
             template_context["reading_value"] = f"{bill_data.reading_value:.2f}"
-            
+
             body = await TemplateLoader.get_template(
                 "email/notificacion_boleta.html",
                 email_title=bill_data.subject,
@@ -94,7 +102,7 @@ class EmailService:
     async def send_water_payment_email(
         email_session: EmailSession,
         email: EmailBase,
-        email_receipt: EmailWaterReceiptBase 
+        email_receipt: EmailWaterReceiptBase,
     ):
         try:
             message = EmailMessage()
@@ -103,14 +111,18 @@ class EmailService:
             message["Subject"] = email.subject
 
             dump_receipt = email_receipt.model_dump()
-            dump_receipt['date_created'] = email_receipt.date_created.strftime('%d/%m/%Y')
-            dump_receipt['date_updated'] = email_receipt.date_updated.strftime('%d/%m/%Y  %H:%M')
+            dump_receipt["date_created"] = email_receipt.date_created.strftime(
+                "%d/%m/%Y"
+            )
+            dump_receipt["date_updated"] = email_receipt.date_updated.strftime(
+                "%d/%m/%Y  %H:%M"
+            )
 
             body = await TemplateLoader.get_template(
                 "email/water_payment.html",
                 **dump_receipt,
                 email_title="Recibo del agua",
-                year=str(datetime.now().year)
+                year=str(datetime.now().year),
             )
             message.set_content(body, subtype="html")
             await email_session.send_message(message)
