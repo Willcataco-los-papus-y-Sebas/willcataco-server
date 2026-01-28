c3285d766e1e1fb022e6351b4ff47aedebe05e90
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-01-28 01:06:49 -0400

    fix: add strftime for dates

--- app/core/time.py
+++ app/core/time.py
@@ -3,5 +3,11 @@ from zoneinfo import ZoneInfo
 
 BOLIVIA = ZoneInfo("America/La_Paz")
 
-def get_time_zone(time: datetime):
-    return time.astimezone(BOLIVIA)
+class TimeBolivia:
+    @staticmethod
+    def get_time_zone(time: datetime, format: str):
+        if time.tzinfo is None:
+            raise ValueError("Time no puede ser un naive") 
+        if time.tzname() != 'UTC':
+            raise ValueError("Time solo puede ser UTC")
+        return time.astimezone(BOLIVIA).strftime(format)
