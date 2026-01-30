7716534f37b03bc090d71fb0baa57ee728fe5105
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-29 21:54:21 -0400

    feat(email): impl send_extra_payment_email service logic

--- app/modules/email/services.py
+++ app/modules/email/services.py
@@ -4,7 +4,7 @@ from email.message import EmailMessage
 from app.core.config import config
 from app.core.email import EmailSession
 from app.core.templates import TemplateLoader
-from app.modules.email.schemas import EmailBase
+from app.modules.email.schemas import EmailBase, ExtraPaymentEmailParams
 
 
 class EmailService:
@@ -64,3 +64,25 @@ class EmailService:
             await email_session.send_message(message)
         except Exception:
             raise
+
+    @staticmethod
+    async def send_extra_payment_email(
+        email_session: EmailSession,
+        payment_data: ExtraPaymentEmailParams,
+    ):
+        message = EmailMessage()
+        message["From"] = config.email_from
+        message["To"] = payment_data.recipient
+        message["Subject"] = payment_data.subject
+        template_context = payment_data.model_dump()
+        template_context["payment_date"] = payment_data.payment_date.strftime("%Y-%m-%d")
+        template_context["amount"] = f"{payment_data.amount:.2f}"
+        
+        body = await TemplateLoader.get_template(
+            "email/confirmacion_pago_extra.mjml",
+            email_title=payment_data.subject,
+            year=str(datetime.now().year),
+            **template_context,
+        )
+        message.set_content(body, subtype="html")
+        await email_session.send_message(message)
