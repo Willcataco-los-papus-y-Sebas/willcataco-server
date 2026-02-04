c4476dcfc32b8fc7901156f48771d63d10e3bdbb
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-02-04 12:04:43 -0400

    feat: add id water measure in water payment email

--- app/modules/email/schemas.py
+++ app/modules/email/schemas.py
@@ -17,6 +17,7 @@ class EmailWaterReceiptBase(BaseModel):
     name_member: str
     last_name_member: str
     ci_member: str
+    id_water_measure: int
     id_payment: int
     consumption: Decimal = Field(ge=0, max_digits=10, decimal_places=2)
     date_created: datetime
