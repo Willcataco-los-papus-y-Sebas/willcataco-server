e46449b52221303f07e8dcf26709b2fa2b77dfa1
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-02-05 19:34:52 -0400

    feat: add format correct function

--- app/core/time.py
+++ app/core/time.py
@@ -1,4 +1,4 @@
-from datetime import datetime,  date
+from datetime import datetime, date, timezone, time, timedelta
 from zoneinfo import ZoneInfo
 
 BOLIVIA = ZoneInfo("America/La_Paz")
@@ -6,24 +6,26 @@ BOLIVIA = ZoneInfo("America/La_Paz")
 class TimeBolivia:
     
     @staticmethod
-    def format_date(date: date):
-        to_bolivia = TimeBolivia.__get_time_zone(date)
+    def format_date(date: datetime):
+        to_bolivia = TimeBolivia.__get_time_zone_datetime(date)
         return to_bolivia.strftime("%d/%m/%Y")
     
     @staticmethod
     def format_datetime(date: datetime):
-        to_bolivia = TimeBolivia.__get_time_zone(date)
+        to_bolivia = TimeBolivia.__get_time_zone_datetime(date)
         return to_bolivia.strftime("%d/%m/%Y %H:%M")
     
     @staticmethod
-    def iso_format_date(date: date):
-        to_bolivia = TimeBolivia.__get_time_zone(date)
-        return to_bolivia.strftime("%Y-%m-%d")
+    def format_correct(date: date):
+        to_datetime = datetime.combine(date, time.min)
+        to_UTC = to_datetime.astimezone(timezone.utc)
+        correct_time = to_UTC + timedelta(hours=4)
+        return correct_time
     
     @staticmethod
-    def __get_time_zone(time: datetime):
+    def __get_time_zone_datetime(time: datetime):
         if time.tzinfo is None:
-            raise ValueError("Time no puede ser un naive") 
+            time = time.replace(tzinfo = timezone.utc) 
         if time.tzname() != 'UTC':
-            raise ValueError("Time solo puede ser UTC")
+            raise ValueError("Parameter 'time' only can be in format UTC")
         return time.astimezone(BOLIVIA)
